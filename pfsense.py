ecc83f87f47716dd486bdbf28203b718faf8cdb3
martin1724 <martinmatyja1724@gmail.com>
2023-06-07 20:49:52 +0200

    bug fix in pfsense handler

--- pfsense.py
+++ pfsense.py
@@ -5,7 +5,7 @@ import subprocess
 from config import PFSENSE_ADDRESS
 
 def add_new_rule(source, port, action="block", interface="wan", protocol="tcp", destination="NET:lan"):
-    command_string = 'ansible-playbook {} -i ./playbook/services.yaml --extra-vars "action_pfsense={} interface_pfsense={} protocol_pfsense={} source_pfsense={} dest_ansible={} port_pfsense={}"'.format(action, interface, protocol, source, destination, port)
+    command_string = 'ansible-playbook {} -i ./playbook/services.yaml --extra-vars "action_pfsense={} interface_pfsense={} protocol_pfsense={} source_pfsense={} dest_ansible={} port_pfsense={}"'.format(PFSENSE_ADDRESS, action, interface, protocol, source, destination, port)
     command_list = command_string.split()
     result_command = subprocess.run(command_list, stdout=subprocess.PIPE).stdout.decode('utf-8')
     if "SUCCESS" not in str(result_command):
